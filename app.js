256e4c0058dc56279e9933cb5932704e6c1d7e17
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-06-23 22:05:40 -0500

    Added semicolons

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 import { add } from './utils';
 
 // define and grab DOM elements
-const addInput1 = document.getElementById('add-input-1')
-const addInput2 = document.getElementById('add-input-2')
-const addButton = document.getElementById('add-button')
-const addResult = document.getElementById('add-result')
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-result');
 
 addButton.addEventListener('click', () => {
     const num1 = Number(addInput1.value);
